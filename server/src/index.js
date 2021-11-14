fc5a7a1aef7bdaafc2f691626e61bdd812a6af9f
Trương Hoàng Sơn <19020420@vnu.edu.vn>
2021-11-14 12:29:41 +0700

    a

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const route = require('./routes/index');
 const db = require('./configs/db/index');
 require('dotenv').config()
+const path = require('path');
 
 app.use(
   express.urlencoded({
@@ -17,6 +18,8 @@ app.use(cookieParser());
 app.use(cors());
 
 db.connect();
+
+app.use(express.static(path.join(__dirname + '/public')));
 
 route(app);
 
