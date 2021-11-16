3cb5c34cff260be8bf0565eefea178e46b9e7b99
dvhata <thaohanh233@gmail.com>
2021-11-16 23:58:14 +0700

    Apartment (api call)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminLogin from "./views/Admin/AdminLoginView/AdminLogin";
-import Apartment from "./views/User/ApartmentView/Apartment";
+import Apartment from "./views/User/ApartmentView/Apartment/Apartment";
 import Login from "./views/User/LoginView/Login";
 import Register from "./views/User/RegisterView/Register";
 import "./App.scss";
 import Home from "./views/User/HomeView/HomeView";
+import ApartmentDetail from "./views/User/ApartmentView/ApartmentDetai/ApartmentDetail";
 
 export default function App() {
   return (
@@ -12,8 +13,9 @@ export default function App() {
       <div className="navbar">
         <Router>
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/apartments" element={<Apartment />} />
+            <Route path="/apartments-detail" element={<ApartmentDetail />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/admin-login" element={<AdminLogin />} />
---
+++ client/src/models/Apartments/ApartmentsData.ts
@@ -0,0 +1,7 @@
+export interface ApartmentData {
+    _id?: string;
+    name?: string;
+    price?: number;
+    images?: string[];
+    slug?: string;
+  }
